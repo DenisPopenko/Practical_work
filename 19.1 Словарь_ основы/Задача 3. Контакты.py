d983354dcf1c757d57e6b631fa54ac87a73b6c88
Denis Popenko <denuc@list.ru>
2023-01-05 13:31:09 +0600

    Initial commit

---
+++ 19.1 Словарь_ основы/Задача 3. Контакты.py
@@ -0,0 +1,9 @@
+contacts = dict()
+#print('Текущие контакты на телефоне:\n', contacts)
+while True:
+    print('\nТекущие контакты на телефоне:')
+    for i in contacts:
+        print(i, contacts[i], '\t')
+    name = input('\nВведите имя: ')
+    number = int(input('Введите номер телефона: '))
+    contacts[name] = number
