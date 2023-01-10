1e6f0182c4be2afbaa92481d5bb78da3797f2789
DenisPopenko <121337283+denispopenko@users.noreply.github.com>
2023-01-10 21:34:43 +0600

    Update Задача 3. Контакты.py

---
+++ 19.2 Методы словарей/Задача 2. Кризис фруктов.py
@@ -0,0 +1,18 @@
+incomes = {
+    'apple': 5600.20,
+    'orange': 3500.45,
+    'banana': 5000.00,
+    'bergamot': 3700.56,
+    'durian': 5987.23,
+    'grapefruit': 300.40,
+    'peach': 10000.50,
+    'pear': 1020.00,
+    'persimmon': 310.00,
+}
+
+print('Общий доход за год составил {} рублей'.format(sum(incomes.values())))
+min_income = min(incomes.values())
+for i in incomes.values() and incomes.keys():
+    if i == min_income:
+        print(i)
+print('Самый маленький доход у {}. Он составляет {} рублей'.format(), min_income))
