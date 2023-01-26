d2943c721033f37b120d2e6b9bdb437c3596c4b2
Denis <denuc@list.ru>
2023-01-26 23:48:23 +0600

    Initial commit

--- 22.2 Модуль os проверки/Задача 2. Поиск файла.py
+++ 22.2 Модуль os проверки/Задача 2. Поиск файла.py
@@ -3,6 +3,9 @@ import os
 # cur_path = input('Ищем в: ')
 # file_name = input('Имя файла: ')
 
+cur_path = 'D:\skillbox\dpo_python_basic'
+file_name = 'main.py'
+
 # print(os.path.abspath('Ситауция по залогам БРК на 26.01.23.docx'))
 
 def find_file(cur_path, file_name):
@@ -10,12 +13,14 @@ def find_file(cur_path, file_name):
         path = os.path.join(cur_path, i_elem)
         if os.path.isfile(path):
             if i_elem == file_name:
-                return path
+                print(path)
         else:
             result = find_file(path, file_name)
             if result:
                 break
+    else:
+        result = None
 
     return result
 
-find_file('D:\Denis\Мои документы', 'Справка по реализации и оплате с расшифровкой по договорам 18.01.23.xlsx')
+print(find_file(cur_path, file_name))
---
+++ 22.3 Базовые операции с файлами open, close, read/Задача 1. Результаты.py
@@ -0,0 +1,21 @@
+import os
+
+file = open(os.path.join('D:\dask\group_1.txt'), 'r', encoding='utf-8')
+summa = 0
+diff = 0
+for i_line in file:
+    info = i_line.split()
+    # print(info)
+    summa += int(info[2])
+    diff -= int(info[2])
+file.close()
+
+file_2 = open('D:\dask\Additional_info\group_2.txt', 'r', encoding='utf-8')
+compose = 1
+for i_line in file_2:
+    info = i_line.split()
+    compose *= int(info[2])
+file_2.close()
+print(summa)
+print(diff)
+print(compose)
