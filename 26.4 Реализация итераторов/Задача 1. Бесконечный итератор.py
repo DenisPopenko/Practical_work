6fd8288d6f1951e8559eacc675b64f9a40092fb7
Denis <denuc@list.ru>
2023-03-06 05:04:19 +0600

    Initial commit

---
+++ 26.4 Реализация итераторов/Задача 1. Бесконечный итератор.py
@@ -0,0 +1,19 @@
+class СountIterator():
+
+    def __init__(self):
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.count += 1
+        return self.count
+
+
+
+my_iter = СountIterator()
+
+for i_elem in my_iter:
+
+    print(i_elem)
